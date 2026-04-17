95aa0d603c67ad3895e8d391f4c0cec6bd5cecb5
Anastasiia Tsynduk <nastya.cunduk@gmail.com>
2026-04-17 17:09:00 +0200

    added test to getCourseDuration

--- test/getCourseDuration.test.tsx
+++ test/getCourseDuration.test.tsx
@@ -1,1 +1,12 @@
-test("checks if duration transformed to string", () => {});
+import getCourseDuration from "@/helpers/getCourseDuration";
+
+test.each([
+    [60, "01:00 hour"],
+    [600, "10:00 hours"],
+    [90, "01:30 hour"],
+    [50, "00:50 hours"],
+    [1, "00:01 hours"],
+    [0, "00:00 hours"],
+])("checks conversion of duration", (duration, result) => {
+    expect(getCourseDuration(duration)).toBe(result);
+});
