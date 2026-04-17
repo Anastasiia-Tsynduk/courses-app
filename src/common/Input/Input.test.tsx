53f19305e599f9dff35379917cf1fc0fb33528fe
Anastasiia Tsynduk <nastya.cunduk@gmail.com>
2026-04-17 15:00:43 +0200

    added new tests for Input component

--- src/common/Input/Input.test.tsx
+++ src/common/Input/Input.test.tsx
@@ -1,54 +1,79 @@
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from "./Input";
 
 describe("Input conponent", () => {
     let handleChange: jest.Mock;
 
+    const renderInput = (props = {}) => {
+        return render(
+            <Input {...defaultProps} {...props} onChange={handleChange} />
+        );
+    };
+
+    const variantMap = {
+        auth: "input-auth",
+        default: "input-default",
+        small: "input-small",
+    };
+
     const defaultProps = {
         labelText: "Email",
         placeholderText: "Enter email",
         value: "",
         onChange: () => {},
+        errorMessage: "Email is required",
     };
 
     beforeEach(() => {
         handleChange = jest.fn();
-        render(
-            <Input
-                {...defaultProps}
-                onChange={handleChange}
-                variant="auth"
-                errorMessage="Email is required"
-            />
-        );
+    });
+
+    afterEach(() => {
+        cleanup();
     });
 
     test("checks if label text is rendered", () => {
+        renderInput();
         expect(screen.getByLabelText("Email")).toBeInTheDocument();
     });
 
     test("checks if placeholder is rendered", () => {
+        renderInput();
         expect(screen.getByPlaceholderText("Enter email")).toBeInTheDocument();
     });
 
     test("checks if input is rendered ", () => {
+        renderInput();
         expect(screen.getByRole("textbox")).toBeInTheDocument();
     });
+
     test("checks if onChange event works", async () => {
+        renderInput();
         const value = "test";
         await userEvent.type(screen.getByRole("textbox"), value);
         expect(handleChange).toHaveBeenCalledTimes(value.length);
     });
-    test("checks if variant transforms to right class name", () => {
-        expect(screen.getByRole("textbox").parentElement).toHaveClass(
-            "input-auth"
-        );
-    });
+
+    test.each(["auth", "default", "small"] as const)(
+        "check if variat has appropriate className",
+        (variant) => {
+            const className = variantMap[variant];
+            renderInput({
+                variant,
+                errorMessage: "Email is required",
+            });
+            expect(screen.getByRole("textbox").parentElement).toHaveClass(
+                className
+            );
+        }
+    );
     test("checks if error message works correctly", () => {
-        expect(screen.getByText("Email is required")).toBeInTheDocument;
+        renderInput();
+        expect(screen.getByText("Email is required")).toBeInTheDocument();
     });
     test("checks error class when errorMessage exists", () => {
+        renderInput();
         expect(screen.getByRole("textbox").parentElement).toHaveClass(
             "has-error"
         );
